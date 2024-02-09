e5dd1efab30bf14e13eb26eaeb4e372413e49e35
NotTheRightGuy <janmejaychatterjee@gmail.com>
2024-02-10 00:47:47 +0530

    Added Login Signup Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import Landing from "./pages/Landing";
+import LogIn from "./pages/LogIn";
 function App() {
-    return <></>;
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Landing />}></Route>
+                    <Route path="/auth/sign-up" element={<SignUp />}></Route>
+                    <Route path="/auth/login" element={<LogIn />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
